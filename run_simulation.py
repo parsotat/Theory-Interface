ece3245928bda71880235593d5afb93895e3f371
Tyler Parsotan <parsotat@umbc.edu>
2023-10-09 03:39:23 -0400

    Modified notebook to reflect most up to date ideas for interfacing with 3ML.

---
+++ run_simulation.py
@@ -0,0 +1,29 @@
+from ronswanson.band_simulation import BandSimulation as Simulation
+from joblib import Parallel, delayed
+import json
+import h5py
+import numpy as np
+from tqdm.auto import tqdm
+from ronswanson import ParameterGrid
+from ronswanson.utils.logging import setup_logger
+from ronswanson.utils import Colors
+from ronswanson.simulation import gather
+log = setup_logger(__name__)
+
+with open('completed_parameters.json', 'r') as f:
+    complete_params = json.load(f)
+pg = ParameterGrid.from_yaml('/Users/tparsotan/Library/CloudStorage/Box-Box/Theory-Interface/parameters.yml')
+n_points = pg.n_points
+def func(i, silent: bool=True):
+    params = pg.at_index(i)
+    if not silent:
+        log.info(f'{params}')
+    for p in complete_params:
+        if np.alltrue(np.array(p) == params):
+            log.debug('parameters already exists in file!')
+            return
+    simulation = Simulation(i, params, pg.energy_grid,'/Users/tparsotan/Library/CloudStorage/Box-Box/Theory-Interface/database.h5')
+    simulation.run()
+iteration = [i for i in range(0, n_points)]
+Parallel(n_jobs=1)(delayed(func)(i) for i in tqdm(iteration, colour=Colors.RED.value, desc='simulating function'))
+gather('/Users/tparsotan/Library/CloudStorage/Box-Box/Theory-Interface/database.h5', 9, clean=True)
